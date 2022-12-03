8fbed40ad261a7edb1ce5428127feeec9841e6d3
lovelydett <xyt@bupt.cn>
2022-12-03 13:08:26 +0800

    [Utilization] gantt graph by niuniu

--- src/utilization/real_trace.R
+++ src/utilization/real_trace.R
@@ -1,9 +1,12 @@
 # Analyse the ground-truth task-core mapping trace of Baidu Apollo
 # Yuting@2022.11.29
 
-# Set working directory
-setwd("/home/tt/Codes/valley_filling/")
+# Import libraries
+library(ggplot2)
 
+# Set working directory
+# setwd("/home/tt/Codes/valley_filling/")
+setwd("/Users/yuting/Codes/valley_filling/")
 
 # Turn E-Epress off
 options(scipen = 999)
@@ -15,6 +18,8 @@ DEBUG <- function(msg) {
     print(msg)
   }
 }
+
+MS_TO_NS <- 1000000
 
 # Load the trace
 load_mainboard_log <- function(file_path) {
@@ -40,31 +45,49 @@ combine_log <- function(log_dir) {
     return(log_df)
 }
 
-draw_gant_graph <- function(df) {
+get_data <- function(df) {
     num_cores <- ncol(df) - 1
     # First sort the data frame by timestamp
     df <- df[order(df$ts), ]
 
-    # Split dataframe by columns
-    
     # Convert each core data to start-end time
-    {
-        for (i in 1:num_cores) {
-            core_name <- paste0("core", i)
-            df_core <- df[, c("ts", core_name)]
-            DEBUG(df_core[1:10, ])
+    res = data.frame()
+    for (i in 1:num_cores) {
+        core_name <- paste0("core", i)
+        df_core <- df[, c("ts", core_name)]
+        
+        for (j in 1:nrow(df_core)) {
+
+            ts = df_core[j, "ts"]
+            if (df_core[j, core_name] == "idle") {
+                next
+            }
+            s <- strsplit(df_core[j, core_name], ":")[[1]]
+            start <- ts - as.integer(s[2]) * MS_TO_NS
+            task <- s[1]
+            end <- ts
+            res <- rbind(res, c(task, start, end, i))
         }
-    }
-
-
-    # Then we need to convert the timestamped data to start-end data
-    # Elliminate the row with four idle cores
-    df <- df[!(df$core1 == "idle" & df$core2 == "idle" & df$core3 == "idle" & df$core4 == "idle"), ]
-
+        
+    } 
+    colnames(res) <- c("task", "start", "end", "core")
+    return(res)
 }
-
 
 # Main
 dir_path <- "./data/schedule_records/1/log/"
 df <- combine_log(dir_path)
-draw_gant_graph(df)
+res <- get_data(df)
+
+res <- res[order(res$start), ]
+part <- res[(3000 * nrow(res) / 10000):(3010 * nrow(res) / 10000), ]
+
+plot_gantt <-  qplot(xmin = start,
+                    xmax = end,
+                    y = core,
+                    colour = core,
+                    geom = "linerange",
+                    data = part)
+
+png(paste0("./output/", "gantt.png"), units = "in", width = 20, height = 1, res = 300)
+plot(plot_gantt)
